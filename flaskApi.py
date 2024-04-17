8bc1ff63fa1af5fa66d29bf1d7aa8765c5faa5fb
Gui027 <guilhermerg1234@gmail.com>
2024-04-17 11:53:27 -0300

    ROTAS TODAS DINÂMICAS CONECTADAS COM O BACKEND, CADASTRANDO E FUNCIONANDO CERTINHO

--- flaskApi.py
+++ flaskApi.py
@@ -44,47 +44,51 @@ def chat_with_data(df, prompt):
     print(f"Resultado do chat: {result}")  # Debug print
     return result
 
-def fetch_data(cnpj, email):
-    print(f"Recebendo dados para CNPJ: {cnpj}, Email: {email}")  # Debug print
-    empresaId = "fb978f9a-4480-45e4-b4dd-fe06a099b487"
-    # URL de configuração (ajuste conforme necessário para a URL da API de configuração)
+def fetch_data(cnpj, email, empresaId="db362971-d85d-458a-af8c-89f2c6e270cf"):
+    print(f"Recebendo dados para CNPJ: {cnpj}, Email: {email}")
+
     config_url = f"http://192.168.0.105:8080/V1/api/empresa/findconfiguracaobyempresaid/{empresaId}"
+    find_urls = f"http://192.168.0.105:8080/V1/api/api/findbyempresa/{empresaId}"
+
     try:
         config_response = requests.get(config_url)
         config_response.raise_for_status()
         config_data = config_response.json()
         base_url = config_data['url_loc_cli'].format(cnpj=cnpj, email=email)
-    except requests.RequestException as e:
-        print(f"Erro ao buscar a URL dinâmica: {e}")  # Debug print
-        return pd.DataFrame(), None
-    
-    try:
+
         response = requests.get(base_url)
         response.raise_for_status()
         data_json = response.json()
-        print(f"Resposta inicial da API: {data_json}")  # Debug print
-        cliente_id = data_json['dados'][0]['id'] if data_json['dados'] else None
-        
-        print(f"Cliente ID: {cliente_id}")  # Debug print
+        print(f"Resposta inicial da API: {data_json}")
 
-        urls = [
-            f"https://fjinfor.ddns.net/fvendas/api/api_sitpedido.php?funcao=get_sitpedido&cliente={cliente_id}",
-            f"https://fjinfor.ddns.net/fvendas/api/api_sit_boleto.php?funcao=get_sitboleto_1&cliente={cliente_id}"
-        ]
+        cliente_id = data_json['dados'][0]['id'] if data_json['dados'] else None
+        nome_cliente = data_json['dados'][0]['nome'] if data_json['dados'] else None
+
+           # Obtendo URLs do segundo endpoint
+        config_response2 = requests.get(find_urls)
+        config_response2.raise_for_status()
+        config_data2 = config_response2.json()
+        urls = [item['url'] for item in config_data2]
 
         dfs = []
         for url in urls:
             response = requests.get(url)
             response.raise_for_status()
-            data_json = response.json()
-            df = json_to_dataframe(data_json)
+            data_json2 = response.json()
+            print(f"Resposta inicial da API 2: {data_json2}")
+            df = json_to_dataframe(data_json2)
             dfs.append(df)
 
-        combined_df = pd.concat(dfs, axis=1)
-        return combined_df, cliente_id
+        if len(dfs) > 1:
+            combined_df = pd.concat(dfs, axis=1, join='inner')
+        else:
+            combined_df = dfs[0] if dfs else pd.DataFrame()
+
+        return combined_df, cliente_id, nome_cliente, empresaId
+
     except requests.RequestException as e:
-        print(f"Erro na requisição: {e}")  # Debug print
-        return pd.DataFrame(), None
+        print(f"Erro na requisição: {e}")
+        return pd.DataFrame(), None, None
 
 @app.route('/chat_with_data', methods=['POST'])
 def api_chat_with_data():
@@ -95,17 +99,37 @@ def api_chat_with_data():
     
     print(f"Dados recebidos - CNPJ: {cnpj}, Email: {email}, Prompt: {prompt}")  # Debug print
     
-    # Primeiro, busque os dados e o ID do cliente
-    df, cliente_id = fetch_data(cnpj, email)
+    df, cliente_id, nome_cliente, empresaId = fetch_data(cnpj, email)
+    
+    print(f"ClienteId DE BAIXO: {cliente_id}")
+    print(f"Nome Cliente DE BAIXO: {nome_cliente}")
+    print(f"EmpresaId: {empresaId}")
     
     if prompt.lower() == "cadastro":
-        return jsonify({"response": "Olá, como posso ajudar?", "cliente_id": cliente_id})
+        return jsonify({"response": "Olá, como posso ajudar?", "cliente_id": cliente_id, "nome_cliente": nome_cliente})
     
     if df.empty:
         print("DataFrame vazio após tentativa de busca.")  # Debug print
         return jsonify({"error": "Nenhum dado encontrado para o cliente especificado"}), 404
     
     result = chat_with_data(df, prompt)
+    
+    # Código para enviar a resposta ao endpoint
+    post_url = "http://192.168.0.105:8080/V1/api/conversa/create"
+    post_body = {
+        "pergunta": prompt,
+        "resposta": result,
+        "cnpj_cliente": cnpj,
+        "nome_cliente": nome_cliente,
+        "empresa": {"id": empresaId}
+    }
+    try:
+        post_response = requests.post(post_url, json=post_body)
+        post_response.raise_for_status()  # Verificar se a requisição foi bem-sucedida
+        print("POST realizado com sucesso.")  # Debug print
+    except requests.RequestException as e:
+        print(f"Erro ao fazer POST para o endpoint: {e}")  # Debug print
+    
     return jsonify({"response": result, "cliente_id": cliente_id})
 
 @app.route('/')
