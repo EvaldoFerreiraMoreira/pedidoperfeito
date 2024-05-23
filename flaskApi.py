3c3beb80ae21ab9d7ecfcad04a779f1ae3fca90a
Gui027 <guilhermerg1234@gmail.com>
2024-05-23 14:38:50 -0300

    Código com várias mensagens na requisição

--- flaskApi.py
+++ flaskApi.py
@@ -1,8 +1,16 @@
+import logging
 from flask import Flask, request, jsonify
 import pandas as pd
 import requests
 import os
 from flask_cors import CORS
+
+# Configuração do logger
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+# Desativa a verificação SSL
+requests.packages.urllib3.disable_warnings()
 
 # Obtém a chave de API da OpenAI a partir das variáveis de ambiente.
 openai_api_key = os.getenv("OPENAI_API_KEY")
@@ -50,11 +58,17 @@ def fetch_data(cnpj, email, empresaId):
     config_url = f"http://192.168.0.105:8080/V1/api/empresa/findconfiguracaobyempresaid/{empresaId}"
     find_urls = f"http://192.168.0.105:8080/V1/api/api/findbyempresa/{empresaId}"
 
+    config_data = None
+    iurls = None
+
     try:
         config_response = requests.get(config_url)
         config_response.raise_for_status()
         config_data = config_response.json()
-        base_url = config_data['url_loc_cli'].format(cnpj=cnpj, email=email)
+        print(f"URL_LOC_CLI: {config_data.get('url_loc_cli', '')}")
+        print(f"CONFIG DATA: {config_data}")
+
+        base_url = config_data.get('url_loc_cli', '').format(cnpj=cnpj, email=email)
 
         response = requests.get(base_url)
         response.raise_for_status()
@@ -74,9 +88,12 @@ def fetch_data(cnpj, email, empresaId):
         config_response2 = requests.get(find_urls)
         config_response2.raise_for_status()
         config_data2 = config_response2.json()
-        urls = [item['url'].format(cliente_id=cliente_id) for item in config_data2]  # Passando o cliente_id dinamicamente
+        iurls = [item.get('url') for item in config_data2] # Iterando sobre o dicionário de urls
+        print(f"URLS: {iurls}")
+        urls = [item.get('url', '').format(cliente_id=cliente_id) for item in config_data2]  # Passando o cliente_id dinamicamente
 
         dfs = []
+        data_jsons = []  # Lista para armazenar os dados json
         for url in urls:
             response = requests.get(url)
             response.raise_for_status()
@@ -84,23 +101,30 @@ def fetch_data(cnpj, email, empresaId):
             print(f"Resposta inicial da API 2: {data_json2}")
             df = json_to_dataframe(data_json2)
             dfs.append(df)
+            data_jsons.append(data_json2)  # Armazenar os dados json
 
         if len(dfs) > 1:
             combined_df = pd.concat(dfs, axis=1, join='inner')
         else:
             combined_df = dfs[0] if dfs else pd.DataFrame()
 
-        return combined_df, cliente_id, nome_cliente, empresaId
+        return combined_df, cliente_id, nome_cliente, empresaId, data_json, data_jsons, config_data, iurls
 
     except requests.RequestException as e:
         print(f"Erro na requisição: {e}")
-        return pd.DataFrame(), None, None, None
+        return pd.DataFrame(), None, None, None, None, None, config_data, iurls
     except ValueError as ve:
         print(f"Valor inválido encontrado: {ve}")
-        return pd.DataFrame(), None, None, None
+        return pd.DataFrame(), None, None, None, None, None, config_data, iurls
+    except AttributeError as ae:
+        print(f"Erro de atributo: {ae}")
+        return pd.DataFrame(), None, None, None, None, None, config_data, iurls
 
 @app.route('/chat_with_data', methods=['POST'])
 def api_chat_with_data():
+    # Registro das informações da requisição
+    logger.info(f"Recebida uma requisição {request.method} para {request.path} com dados {request.json}")
+
     content = request.json
     cnpj = content.get('cnpj')
     email = content.get('email')
@@ -109,10 +133,18 @@ def api_chat_with_data():
     
     print(f"Dados recebidos - CNPJ: {cnpj}, Email: {email}, Prompt: {prompt}, empresaId: {empresaId}")  # Debug print
     
-    df, cliente_id, nome_cliente, empresaId = fetch_data(cnpj, email, empresaId)
+    df, cliente_id, nome_cliente, empresaId, data_json, data_json2, config_data, iurls = fetch_data(cnpj, email, empresaId)
     
     if cliente_id is None:
-        return jsonify({"error": "clienteId não encontrado"}), 404
+        return jsonify({"error": "clienteId não encontrado", "resposta da api 1": data_json, "resposta da api 2": data_json2, "URL_LOC_CLI": config_data, "URLS": iurls}), 404
+
+    if empresaId is None:
+        if config_data is not None:
+            url_loc_cli = config_data.get('url_loc_cli', '')
+        else:
+            url_loc_cli = None
+        
+        return jsonify({"error": "empresaId não encontrado", "resposta da api 1": data_json, "resposta da api 2": data_json2, "URL_LOC_CLI": url_loc_cli, "URLS": iurls}), 404
     
     print(f"ClienteId DE BAIXO: {cliente_id}")
     print(f"Nome Cliente DE BAIXO: {nome_cliente}")
@@ -143,7 +175,7 @@ def api_chat_with_data():
     except requests.RequestException as e:
         print(f"Erro ao fazer POST para o endpoint: {e}")  # Debug print
     
-    return jsonify({"response": result, "cliente_id": cliente_id})
+    return jsonify({"response": result, "cliente_id": cliente_id, "resposta da api 1": data_json, "resposta da api 2": data_json2, "URL_LOC_CLI": config_data['url_loc_cli'], "URLS": iurls})
 
 @app.route('/')
 def home():
